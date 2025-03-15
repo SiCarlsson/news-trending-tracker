d7c5b2ffe9c8366afe042f630241034e8f44e89c
Simon Carlsson <sicar@kth.se>
2025-03-15 14:16:28 +0100

    Comments and minor updates

--- scraper/news_scraper/news_scraper/spiders/svt_spider.py
+++ scraper/news_scraper/news_scraper/spiders/svt_spider.py
@@ -1,26 +1,56 @@
 import scrapy
+import re
+
 from news_scraper.items import NewsScraperItem
 
 
 class SVTSpider(scrapy.Spider):
     name = "svt"
+    allowed_domains = ["svt.se"]
     start_urls = [
         "https://www.svt.se/nyheter/ekonomi/",
-        # 'https://www.svt.se/nyheter/svtforum/',
-        # 'https://www.svt.se/nyheter/granskning/',
-        # 'https://www.svt.se/nyheter/inrikes/',
-        # 'https://www.svt.se/kultur/',
-        # 'https://www.svt.se/nyheter/utrikes/'
+        'https://www.svt.se/nyheter/svtforum/',
+        'https://www.svt.se/nyheter/granskning/',
+        'https://www.svt.se/nyheter/inrikes/',
+        'https://www.svt.se/kultur/',
+        'https://www.svt.se/nyheter/utrikes/'
     ]
 
     def parse(self, response):
-        # Extract each article
+        """
+        Extracts article titles and URLs from the response.
+
+        Args:
+            response (scrapy.http.Response): The page response to parse.
+
+        Yields:
+            dict: A dictionary containing the title and URL of each article.
+        """
+        site_name = "SVT"
+
         articles = response.xpath('//*[@id="innehall"]/div/section/ul//li/article/a')
 
         for article in articles:
-            item = NewsScraperItem()
+            title = article.xpath("@title").get()
+            url = article.xpath("@href").get()
 
-            item["title"] = article.xpath("@title").get()
-            item["url"] = response.urljoin(article.xpath("@href").get())
+            # Tokenize the title into words
+            tokens = self.tokenize_title(title)
 
-            yield item
+            for token in tokens:
+                yield NewsScraperItem(word=token, url=url, site_name=site_name)
+
+    def tokenize_title(self, title):
+        """
+        Tokenizes a title by cleaning and splitting it into words.
+        
+        Args:
+            title (str): The title to tokenize.
+        
+        Returns:
+            list: A list of words (tokens).
+        """
+        title = title.strip().lower()
+        title = re.sub(r"[^\w\s]", "", title)
+        tokens = title.split()
+        return tokens
