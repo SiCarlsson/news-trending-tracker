03ee1c5189301e4dece34240c73fe3cb9abdd258
Simon Carlsson <sicar@kth.se>
2025-03-15 17:29:02 +0100

    Enhanced spider

--- scraper/news_scraper/news_scraper/items.py
+++ scraper/news_scraper/news_scraper/items.py
@@ -1,12 +1,23 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
+class WebsiteItem(scrapy.Item):
+    website_id = scrapy.Field()
+    website_name = scrapy.Field()
+    website_url = scrapy.Field()
 
-class NewsScraperItem(scrapy.Item):
-    word = scrapy.Field()
-    url = scrapy.Field()
-    site_name = scrapy.Field()
+class WordItem(scrapy.Item):
+    word_id = scrapy.Field()
+    word_text = scrapy.Field()
+
+class ArticleItem(scrapy.Item):
+    article_id = scrapy.Field()
+    website_id = scrapy.Field()
+    article_title = scrapy.Field()
+    article_url = scrapy.Field()
+
+class OccurrenceItem(scrapy.Item):
+    occurrence_id = scrapy.Field()
+    word_id = scrapy.Field()
+    website_id = scrapy.Field()
+    article_id = scrapy.Field()
+    timestamp = scrapy.Field()
--- scraper/news_scraper/news_scraper/spiders/svt_spider.py
+++ scraper/news_scraper/news_scraper/spiders/svt_spider.py
@@ -1,7 +1,9 @@
 import scrapy
 import re
+import uuid
+from datetime import datetime
 
-from news_scraper.items import NewsScraperItem
+from news_scraper.items import WebsiteItem, ArticleItem, WordItem, OccurrenceItem
 
 
 class SVTSpider(scrapy.Spider):
@@ -9,12 +11,22 @@ class SVTSpider(scrapy.Spider):
     allowed_domains = ["svt.se"]
     start_urls = [
         "https://www.svt.se/nyheter/ekonomi/",
-        'https://www.svt.se/nyheter/svtforum/',
-        'https://www.svt.se/nyheter/granskning/',
-        'https://www.svt.se/nyheter/inrikes/',
-        'https://www.svt.se/kultur/',
-        'https://www.svt.se/nyheter/utrikes/'
+        # 'https://www.svt.se/nyheter/svtforum/',
+        # 'https://www.svt.se/nyheter/granskning/',
+        # 'https://www.svt.se/nyheter/inrikes/',
+        # 'https://www.svt.se/kultur/',
+        # 'https://www.svt.se/nyheter/utrikes/'
     ]
+
+    # Store known words to avoid duplicates
+    word_cache = {}
+
+    def __init__(self, *args, **kwargs):
+        super(SVTSpider, self).__init__(*args, **kwargs)
+
+        self.website_id = str(uuid.uuid4())
+        self.website_name = "SVT"
+        self.website_url = "https://www.svt.se"
 
     def parse(self, response):
         """
@@ -26,23 +38,60 @@ class SVTSpider(scrapy.Spider):
         Yields:
             dict: A dictionary containing the title and URL of each article.
         """
-        site_name = "SVT"
 
-        articles = response.xpath('//*[@id="innehall"]/div/section/ul//li/article/a')
+        # Yield website item only once per spider
+        if response.url == self.start_urls[0]:
+            website_item = WebsiteItem()
+            website_item["website_id"] = self.website_id
+            website_item["website_name"] = self.website_name
+            website_item["website_url"] = self.website_url
+
+            yield website_item
+
+        # Fetch all articles on current page
+        articles = response.xpath('//*[@id="innehall"]/div/section/ul//li')
 
         for article in articles:
-            title = article.xpath("@title").get()
-            url = article.xpath("@href").get()
+            article_id = str(uuid.uuid4())
+            article_title = article.xpath(".//article/a/@title").get()
+            article_url = article.xpath(".//article/a/@href").get()
 
-            # Tokenize the title into words
-            tokens = self.tokenize_title(title)
+            article_item = ArticleItem()
+            article_item["article_id"] = article_id
+            article_item["website_id"] = self.website_id
+            article_item["article_title"] = article_title
+            article_item["article_url"] = article_url
+            yield article_item
 
-            for token in tokens:
-                yield NewsScraperItem(word=token, url=url, site_name=site_name)
+            words = self.tokenize_title(article_title)
+            for word in words:
+                word_lower = word.lower()
+
+                if (word_lower not in self.word_cache):
+                    word_id = str(uuid.uuid4())
+                    self.word_cache[word_lower] = word_id
+
+                    word_item = WordItem()
+                    word_item["word_id"] = word_id
+                    word_item["word_text"] = word_lower
+                    yield word_item
+                else:
+                    word_id = self.word_cache[word_lower]
+
+                occurrence_id = str(uuid.uuid4())
+                occurrence_timestamp = datetime.now().isoformat()
+
+                occurrence_item = OccurrenceItem()
+                occurrence_item['occurrence_id'] = occurrence_id
+                occurrence_item['word_id'] = word_id
+                occurrence_item['website_id'] = self.website_id
+                occurrence_item["article_id"] = article_id
+                occurrence_item['timestamp'] = occurrence_timestamp
+                yield occurrence_item
 
     def tokenize_title(self, title):
         """
-        Tokenizes a title by cleaning and splitting it into words.
+        Tokenizes a title by cleaning and splitting it into words. Filters out special characters and blank words.
         
         Args:
             title (str): The title to tokenize.
@@ -50,6 +99,9 @@ class SVTSpider(scrapy.Spider):
         Returns:
             list: A list of words (tokens).
         """
+        if not title:
+            return []
+
         title = title.strip().lower()
         title = re.sub(r"[^\w\s]", "", title)
         tokens = title.split()
