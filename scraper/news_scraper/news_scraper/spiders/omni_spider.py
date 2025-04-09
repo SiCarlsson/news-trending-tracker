a9ee506da5538818c5abc1efe89870eca9727262
Simon Carlsson <operator_ax.09@icloud.com>
2025-04-09 23:03:50 +0200

    omni headers xpath

--- scraper/news_scraper/news_scraper/spiders/omni_spider.py
+++ scraper/news_scraper/news_scraper/spiders/omni_spider.py
@@ -1,17 +1,18 @@
 import scrapy
 
+
 class OmniSpider(scrapy.Spider):
     name = "omni"
     allowed_domains = ["omni.se"]
-    start_urls = [
-        "https://www.omni.se/senaste"
-    ]
+    start_urls = ["https://www.omni.se/senaste"]
 
     def __init__(self, *args, **kwargs):
         super(OmniSpider).__init__(*args, **kwargs)
 
         self.website_name = "Omni"
         self.website_url = "https://omni.se"
-    
+
     def parse(self, response):
-        pass
+        headlines = response.xpath(
+            "/html/body/main/div/div[2]/div/div[1]/div[1]/div/div//article/div/a/div/div[1]/h2//text()"
+        ).getall()
