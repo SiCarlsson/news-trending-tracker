99505a56e0c35d2aa7aed4dc04a177bffec0c67a
Simon Carlsson <sicar@kth.se>
2025-06-09 22:37:42 +0200

    Extended BaseSpider

--- scraper/news_scraper/news_scraper/spiders/omni_spider.py
+++ scraper/news_scraper/news_scraper/spiders/omni_spider.py
@@ -1,8 +1,5 @@
-import uuid
+# filepath: /Users/scarlsson/Programmering/Current projects/news-trending-tracker/scraper/news_scraper/news_scraper/spiders/omni_spider.py
 import scrapy
-from datetime import datetime
-
-from news_scraper.items import WebsiteItem, ArticleItem, WordItem, OccurrenceItem
 from .base_spider import BaseSpider
 
 
@@ -12,32 +9,12 @@ class OmniSpider(BaseSpider):
     start_urls = ["https://www.omni.se/senaste"]
 
     def __init__(self, *args, **kwargs):
-        super(OmniSpider).__init__(*args, **kwargs)
-        self.website_id = str(uuid.uuid4())
+        super(OmniSpider, self).__init__(*args, **kwargs)
         self.website_name = "Omni"
         self.website_url = "https://omni.se"
 
         self.ad_words = ["annons"]
         self.promotion_words = ["erbjudande", "omni mer"]
-
-    def start_requests(self):
-        """
-        Generate initial requests for the spider and yield website metadata.
-        
-        Yields:
-            WebsiteItem: Contains website metadata (id, name, url)
-            Request: HTTP requests to the start_urls for parsing
-        """
-        # First yield the website item
-        website_item = WebsiteItem()
-        website_item["website_id"] = self.website_id
-        website_item["website_name"] = self.website_name
-        website_item["website_url"] = self.website_url
-        yield website_item
-
-        # Then generate requests for the start URLs
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         """
@@ -56,46 +33,21 @@ class OmniSpider(BaseSpider):
 
         for article in articles:
             ad = article.xpath(".//div/div[1]/div/a/span[3]/text()").get()
-            if ad and  ad.lower() in self.ad_words:
+            if ad and ad.lower() in self.ad_words:
                 continue
 
             omni_promotion = article.xpath(".//div/div[1]/div[1]/text()").get()
             if omni_promotion and omni_promotion.lower() in self.promotion_words:
                 continue
 
-            article_id = str(uuid.uuid4())
             article_title = article.xpath(
                 ".//div/div[2]/article/div/a/div/div[1]/h2/text()"
             ).get()
             article_url = article.xpath(".//div/div[2]/article/div/a/@href").get()
 
-            # Create article item
-            article_item = ArticleItem()
-            article_item["article_id"] = article_id
-            article_item["website_id"] = self.website_id
-            article_item["article_title"] = article_title
-            article_item["article_url"] = article_url
+            # Create article item using base spider method
+            article_id, article_item = self.create_article_item(article_title, article_url)
             yield article_item
 
-            words = self.tokenize_title(article_title)
-            for word in words:
-                word_lower = word.lower()
-
-                # Create word item
-                word_id = str(uuid.uuid4())
-                word_item = WordItem()
-                word_item["word_id"] = word_id
-                word_item["word_text"] = word_lower
-                yield word_item
-
-                # Create occurrence item
-                occurrence_id = str(uuid.uuid4())
-                occurrence_timestamp = datetime.now().isoformat()
-
-                occurrence_item = OccurrenceItem()
-                occurrence_item["occurrence_id"] = occurrence_id
-                occurrence_item["word_id"] = word_id
-                occurrence_item["website_id"] = self.website_id
-                occurrence_item["article_id"] = article_id
-                occurrence_item["timestamp"] = occurrence_timestamp
-                yield occurrence_item
+            # Process words using base spider method
+            yield from self.process_article_words(article_title, article_id)
--- scraper/news_scraper/news_scraper/spiders/svt_spider.py
+++ scraper/news_scraper/news_scraper/spiders/svt_spider.py
@@ -1,8 +1,4 @@
 import scrapy
-import uuid
-
-from datetime import datetime
-from news_scraper.items import WebsiteItem, ArticleItem, WordItem, OccurrenceItem
 from .base_spider import BaseSpider
 
 
@@ -20,28 +16,8 @@ class SVTSpider(BaseSpider):
 
     def __init__(self, *args, **kwargs):
         super(SVTSpider, self).__init__(*args, **kwargs)
-        self.website_id = str(uuid.uuid4())
         self.website_name = "SVT"
         self.website_url = "https://www.svt.se"
-
-    def start_requests(self):
-        """
-        Generate initial requests for the spider and yield website metadata.
-        
-        Yields:
-            WebsiteItem: Contains website metadata (id, name, url)
-            Request: HTTP requests to the start_urls for parsing
-        """
-        # First yield the website item
-        website_item = WebsiteItem()
-        website_item["website_id"] = self.website_id
-        website_item["website_name"] = self.website_name
-        website_item["website_url"] = self.website_url
-        yield website_item
-        
-        # Then generate requests for the start URLs
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         """
@@ -53,40 +29,14 @@ class SVTSpider(BaseSpider):
             WordItem: An item for each word in the article title.
             OccurrenceItem: An item linking words to articles and websites.
         """
-        # Fetch all articles on current page
+        # Fetch all articles
         articles = response.xpath('//*[@id="innehall"]/div/section/ul//li')
 
         for article in articles:
-            article_id = str(uuid.uuid4())
             article_title = article.xpath(".//article/a/@title").get()
             article_url = article.xpath(".//article/a/@href").get()
 
-            # Create article item
-            article_item = ArticleItem()
-            article_item["article_id"] = article_id
-            article_item["website_id"] = self.website_id
-            article_item["article_title"] = article_title
-            article_item["article_url"] = article_url
+            article_id, article_item = self.create_article_item(article_title, article_url)
             yield article_item
 
-            words = self.tokenize_title(article_title)
-            for word in words:
-                word_lower = word.lower()
-
-                # Create word item
-                word_id = str(uuid.uuid4())
-                word_item = WordItem()
-                word_item["word_id"] = word_id
-                word_item["word_text"] = word_lower
-                yield word_item
-
-                # Create occurrence item
-                occurrence_id = str(uuid.uuid4())
-                occurrence_timestamp = datetime.now().isoformat()
-                occurrence_item = OccurrenceItem()
-                occurrence_item["occurrence_id"] = occurrence_id
-                occurrence_item["word_id"] = word_id
-                occurrence_item["website_id"] = self.website_id
-                occurrence_item["article_id"] = article_id
-                occurrence_item["timestamp"] = occurrence_timestamp
-                yield occurrence_item
+            yield from self.process_article_words(article_title, article_id)
