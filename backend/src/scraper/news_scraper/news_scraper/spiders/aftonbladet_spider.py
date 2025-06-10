faf7c79cbae90d9021fc08ac4e75df9be58b81a0
Simon Carlsson <sicar@kth.se>
2025-06-10 22:44:55 +0200

    Aftonbladet spider implemented

---
+++ backend/src/scraper/news_scraper/news_scraper/spiders/aftonbladet_spider.py
@@ -0,0 +1,42 @@
+import scrapy
+from .base_spider import BaseSpider
+
+
+class AftonbladetSpider(BaseSpider):
+    name = "aftonbladet"
+    allowed_domains = ["aftonbladet.se"]
+    start_urls = ["https://www.aftonbladet.se/senastenytt"]
+
+    def __init__(self, *args, **kwargs):
+        super(AftonbladetSpider, self).__init__(*args, **kwargs)
+        self.website_name = "Aftonbladet"
+        self.website_url = "https://www.aftonbladet.se"
+
+    def parse(self, response):
+        """
+        Extracts article titles and URLs from the response.
+        Args:
+            response (scrapy.http.Response): The page response to parse.
+        Yields:
+            ArticleItem: An item containing article details.
+            WordItem: An item for each word in the article title.
+            OccurrenceItem: An item linking words to articles and websites.
+        """
+        # Fetch all articles
+        articles = response.xpath('//*[@id="main"]/div[1]/main/section/div')
+
+        for article in articles:
+            article_title = article.xpath(".//a/div/div[2]/h2/text()").get()
+            if article_title is None:
+                article_title = article.xpath(".//a/div/div[1]/h2/text()").get()
+
+            article_url = article.xpath(".//a/@href").get()
+
+            # Ads give None values 
+            if article_title is None or article_url is None:
+                continue
+
+            article_item = self.create_article_item(article_title, article_url)
+            yield article_item
+
+            yield from self.process_article_words(article_title, article_url)
