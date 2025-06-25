04479e4df80140c285c00490a3d6e69edde03e16
Simon Carlsson <operator_ax.09@icloud.com>
2025-06-25 23:50:08 +0200

    Fixed null items from SVT

--- scraper/news_scraper/news_scraper/spiders/svt_spider.py
+++ scraper/news_scraper/news_scraper/spiders/svt_spider.py
@@ -36,6 +36,10 @@ class SVTSpider(BaseSpider):
             article_title = article.xpath(".//article/a/@title").get()
             article_url = article.xpath(".//article/a/@href").get()
 
+            if not article_title or not article_url:
+                self.logger.warning("Missing article title or URL on page: %s", response.url)
+                continue
+
             article_item = self.create_article_item(article_title, article_url)
             yield article_item
 
