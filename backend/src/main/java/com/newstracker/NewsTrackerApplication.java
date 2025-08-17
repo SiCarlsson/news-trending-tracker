f33100ffe79df90d01a689822709de8d5d6415c5
Simon Carlsson <operator_ax.09@icloud.com>
2025-08-17 22:07:08 +0200

    added Spring Boot Backend

---
+++ backend/src/main/java/com/newstracker/NewsTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.newstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NewsTrackerApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/newstracker/NewsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
